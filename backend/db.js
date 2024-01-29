56b4142620662c29b3027b4a58dd40a4c42d8fbe
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-01-29 23:16:52 +0530

    add balance/account schema

--- backend/db.js
+++ backend/db.js
@@ -6,6 +6,16 @@ const userSchema = mongoose.Schema({
     firstname:String,
     lastname:String
 })
+const balanceSchema = mongoose.Schema({
+  userId: {
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'User'
+  },
+  balance: {
+    type:Number,
+    default: 0
+}
+})
 userSchema.pre('save',async function(next){
     if(!this.isModified("password")) return next()
     this.password =await bcrypt.hash(this.password , 10)
@@ -16,4 +26,6 @@ userSchema.pre('save',async function(next){
 userSchema.methods.isPassword = async function(password){
   return await bcrypt.compare(password  , this.password)
 }
-module.exports = mongoose.model('User' , userSchema)
+const User = mongoose.model('User' , userSchema)
+const Balance = mongoose.model('Balance' , balanceSchema)
+module.exports = {User , Balance}
