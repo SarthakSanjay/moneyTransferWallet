ee81ec334c2f984f13a7587228035c08071868eb
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-02-01 23:48:54 +0530

    add transaction component

---
+++ frontend/src/components/Transactions.jsx
@@ -0,0 +1,40 @@
+import axios from 'axios'
+import Cookies from 'js-cookie'
+import React, { useEffect, useState } from 'react'
+import { capitalizeFirstLetter } from '../utils/usefullFuncitons'
+
+const Transactions = () => {
+    const [transactions , setTransactions] = useState([])
+    useEffect(()=>{
+        axios.get(`http://localhost:3000/api/v1/user/transaction`,{
+            headers:{
+                'Authorization': `Bearer ${Cookies.get('token')}`
+            }
+        }).then(res =>{
+            setTransactions(res.data.transactions)
+        })
+    },[])
+  return (
+    <div className='h-1/2 w-full px-4 font-semibold'>
+    <div className='h-18 w-full flex justify-between px-4 my-1 items-center dark:text-white dark:border-b dark:border-b-emerald-400'>
+        <span className='w-1/4 text-center hidden md:block'>TXID</span>
+        <span className='w-1/4 text-center'>Name</span>
+        <span className='w-1/4 text-center'>Status</span>
+        <span className='w-1/4 text-center'>Date</span>
+        <span className='w-1/4 text-center'>Amount</span>
+    </div>
+        {transactions.map((transaction)=>{
+            return <div className='h-18 w-full flex justify-between px-4 my-1 items-center' key={transaction._id}>
+                        <span className='w-1/4 text-center font-light text-sm dark:text-[#d8e9a8] hidden md:block'>{transaction._id}</span>
+                        <span className='w-1/4 text-center dark:text-[#d8e9a8]'>{capitalizeFirstLetter(transaction.receiver.firstname)} {capitalizeFirstLetter(transaction.receiver.lastname)}</span>
+                        <span className='w-1/4 text-center text-emerald-700 dark:text-emerald-500 '>send</span>
+                        <span className='w-1/4 text-center dark:text-white '>{new Date(transaction.createdAt).toLocaleDateString()}</span>
+                        <span className='w-1/4 text-center dark:text-orange-500'>${transaction.amount}</span>
+                    </div>
+        })}
+
+    </div>
+  )
+}
+
+export default Transactions
