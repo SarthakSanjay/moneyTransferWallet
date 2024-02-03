b3470d6813b3e4368bf928724daf89202ef8c284
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-02-03 23:56:43 +0530

    add routing to the app using react-router dom

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,44 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom";
+import SignUp from './pages/SignUp.jsx';
+import Dashboard from './pages/Dashboard.jsx';
+import SignIn from './pages/SignIn.jsx';
+import Send from './pages/Send.jsx';
+import Users from './components/Users.jsx';
+import Transactions from './components/Transactions.jsx';
+
+const router = createBrowserRouter([
+  {
+    path:"/" , element: <App />
+  },
+  {
+    path:"/signup" , element: <SignUp />
+  },
+  {
+    path:"/signin" , element: <SignIn />
+  },
+  {
+    path:"/dashboard" , element: <Dashboard />,
+    children:[{
+      path:"users" ,element: <Users />
+    },{
+      path:"transactions" ,element: <Transactions />
+    }]
+  },
+  {
+    path:"/send" , element: <Send />
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+  <RouterProvider router={router} >
     <App />
+  </RouterProvider>
   </React.StrictMode>,
 )
