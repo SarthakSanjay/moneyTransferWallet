ce131d0d80650fbd9434c14528c9ca49f40f829d
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-02-03 23:52:04 +0530

    refactor code style change

--- frontend/src/components/Balance.jsx
+++ frontend/src/components/Balance.jsx
@@ -13,8 +13,15 @@ const Balance = () => {
             setBalance(res.data.balance)
         })
     },[])
+    
   return (
-    <div className='h-20 w-full flex justify-start items-center px-4'>Balance $ {balance.toFixed(2)}</div>
+    <div className='h-20 w-full flex justify-start items-center text-xl px-4 gap-2 dark:text-[#d8e9a8]'>Balance $ {balance.toFixed(2)}
+    <span className={`w-max px-4   ${
+    balance > 500 ? 'text-green-500' :
+    balance > 0 ? 'text-yellow-600' :
+    balance <= 0 ? 'text-red-500' : ''
+  }`}>Low Balance</span>
+    </div>
   )
 }
 
