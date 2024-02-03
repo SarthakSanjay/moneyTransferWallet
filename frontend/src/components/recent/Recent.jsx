27512e6f4ba3ce3254c49ba4e7bdf51cb01126b0
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-02-03 23:50:41 +0530

    add recent component

---
+++ frontend/src/components/recent/Recent.jsx
@@ -0,0 +1,44 @@
+import axios from 'axios'
+import Cookies from 'js-cookie'
+import React, { useEffect, useState } from 'react'
+import RecentLoader from '../../loaders/RecentLoader';
+import RecentSquare from './RecentSquare';
+import { useNavigate } from 'react-router-dom';
+
+const Recent = () => {
+    const [transactions , setTransactions] = useState([])
+    const [loading , setLoading] = useState(true)
+    const navigate = useNavigate()
+    useEffect(()=>{
+      setTimeout(()=>{
+        axios.get(`http://localhost:3000/api/v1/user/transaction`,{
+            headers:{
+                'Authorization': `Bearer ${Cookies.get('token')}`
+            }
+        }).then(res =>{
+            setTransactions(res.data.transactions)
+            setLoading(false)
+        })
+      },1000)
+    },[])
+  return (
+    <>
+        {loading ? <RecentLoader /> : <div className='h-max  lg:h-1/2  w-full md:w-1/2 p-4 text-black font-semibold  dark:text-white '>
+        <h1 className='mb-2'>Recent Activity</h1>
+        <div className='flex'>
+        <div className='w-max mr-2 flex flex-row-reverse justify-end gap-2   '>
+          {transactions.slice(-3).map((transx , index)=>{
+              if(index > 3) return
+              return <RecentSquare key={transx._id} transx={transx} />
+          })}
+       </div>
+        <button onClick={() => {navigate('/dashboard/transactions')}}>
+          <RecentSquare text={'more'} />
+        </button>
+        </div>
+    </div>}
+    </>
+  )
+}
+
+export default Recent
