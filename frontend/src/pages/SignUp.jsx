07945e998cf57097ef2ac21f17e002594c93db2c
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-02-04 01:47:30 +0530

    add dark mode

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 import Cookies from 'js-cookie'
+import DarkMode from '../components/buttons/DarkMode'
 const SignUp = () => {
     const [firstName , setFirstName] = useState('')
     const [lastName , setLastName] = useState('')
@@ -36,37 +37,38 @@ const SignUp = () => {
           })
     }
   return (
-    <div className=' h-screen w-screen flex justify-center items-center bg-black/40'>
-        <div className='h-max w-[400px] rounded-md border py-3 px-5 bg-white'>
-            <h1 className='text-center font-bold text-3xl'>Sign Up</h1>
+    <div className=' h-screen w-screen flex justify-center items-center dark:bg-[#191a19] bg-black/40'>
+    <div className='absolute top-10 '><DarkMode /></div>
+        <div className='h-max w-[400px] rounded-md border dark:border-none py-3 px-5 bg-white dark:bg-emerald-600/90 dark:text-white'>
+            <h1 className='text-center font-bold text-3xl '>Sign Up</h1>
             <p className='text-center my-2 px-4'>Enter your information and create an account</p>
            <div className='flex flex-col justify-center h-[450px] '>
            <label>First Name</label>
             <input 
-            className='border h-10 outline-none px-2 my-2 rounded-md' 
+            className='border h-10 outline-none px-2 my-2 rounded-md dark:bg-black/50 dark:border-none' 
             placeholder='John' 
             onChange={handleFirstName}
             />
             <label>Last Name</label>
             <input 
-            className='border h-10 outline-none px-2 my-2 rounded-md' 
+            className='border h-10 outline-none px-2 my-2 rounded-md dark:bg-black/50 dark:border-none' 
             placeholder='Doe'
             onChange={handleLastName}
             />
             <label>Email</label>
             <input 
-            className='border h-10 outline-none px-2 my-2 rounded-md' 
+            className='border h-10 outline-none px-2 my-2 rounded-md dark:bg-black/50 dark:border-none' 
             placeholder='johndoe@example.com'
             onChange={handleEmail}
             />
              <label>Password </label>
             <input 
-            className='border h-10 outline-none px-2 my-2 rounded-md' 
+            className='border h-10 outline-none px-2 my-2 rounded-md dark:bg-black/50 dark:border-none' 
             placeholder='John' 
             onChange={handlePassword}
             />
             <button 
-            className='bg-black text-white h-10 rounded-md my-2'
+            className='bg-black dark:bg-[#191a19] text-white h-10 rounded-md my-2'
             onClick={handleClick}
             >Sign up</button>
            </div>
