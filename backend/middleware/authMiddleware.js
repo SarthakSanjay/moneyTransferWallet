0a24eac9bb6f855831e8dc144e520814b614e1e7
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-02-03 23:11:50 +0530

    refactor code

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -2,11 +2,9 @@ const { JWT_SECRET } = require("../config");
 const jwt = require("jsonwebtoken");
 const authMiddlware = async(req,res,next) =>{
     const authHeader = req.headers.authorization;
-console.log(authHeader);
-if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    return res.status(403).json({});
-   
-}
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(403).json({});
+    }
 const token = authHeader.split(' ')[1];
     try {
         const decoded = jwt.verify(token, JWT_SECRET);
