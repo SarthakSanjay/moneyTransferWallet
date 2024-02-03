b327552fe170eebf47ff505e3f3956a20d1ea328
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-02-03 23:54:43 +0530

    add random color array

---
+++ frontend/src/utils/colors.js
@@ -0,0 +1,27 @@
+const tailwindColors = [
+    'bg-red-500',
+    'bg-blue-500',
+    'bg-green-500',
+    'bg-yellow-500',
+    'bg-purple-500',
+    'bg-indigo-500',
+    'bg-pink-500',
+    'bg-teal-500',
+    'bg-orange-500',
+    'bg-gray-500',
+    'bg-red-700',
+    'bg-blue-700',
+    'bg-green-700',
+    'bg-yellow-700',
+    'bg-purple-700',
+    'bg-indigo-700',
+    'bg-pink-700',
+    'bg-teal-700',
+    'bg-orange-700',
+    'bg-gray-700',
+
+  ];
+  export default tailwindColors
+
+  
+  
