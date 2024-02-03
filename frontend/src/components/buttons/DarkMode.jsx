f11f5adb2d6215f645f683ae1bbe9d3401089771
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-02-03 23:46:18 +0530

    add dark mode btn

---
+++ frontend/src/components/buttons/DarkMode.jsx
@@ -0,0 +1,20 @@
+import Cookies from 'js-cookie'
+import React, { useEffect, useState } from 'react'
+import { CiDark } from 'react-icons/ci'
+import { MdOutlineLightMode } from 'react-icons/md'
+import ProfileButtons from '../profile/ProfileButtons'
+import useDarkMode from '../../hooks/useDarkMode'
+
+const DarkMode = () => {
+    
+  const {theme ,toggleDarkMode} = useDarkMode()
+   
+  return (
+    <button className='mx-5 border-2 text-xl h-10 w-10 flex justify-center items-center font-semibold rounded-full border-black dark:border-white' onClick={toggleDarkMode}>
+    {theme === 'dark'  ?<MdOutlineLightMode />: <CiDark /> }
+
+</button>
+  )
+}
+
+export default DarkMode
