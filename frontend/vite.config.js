8caca83c527385c7503a77b633574fffc5a119f2
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-02-06 13:29:45 +0530

    removed createProxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { createProxy } from 'vite-plugin-mock'; // Add this line
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), createProxy()], // Add createProxy() to the plugins array
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://money-transfer-wallet-backend-pdsa1jg3s-sarthaksanjay.vercel.app',
-        changeOrigin: true,
-      },
-    },
-  },
-});
+  plugins: [react()],
+})
