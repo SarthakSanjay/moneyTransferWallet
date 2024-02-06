b1786db09db5bf96423938b4cc35fc27ca2e8c7a
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-02-06 23:06:45 +0530

    changed style of modal

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -13,7 +13,7 @@ const Modal = ({text}) => {
   return (
     <div className='absolute h-1/3 w-4/5 z-20 dark:bg-black bg-blue-600/90 rounded-lg text-black dark:text-white flex flex-col items-center justify-center'>
     <h1 className='text-center'>{text}</h1>
-    <button className='bg-black dark:bg-white rounded-md px-3 py-1' onClick={handleClick}>Okay</button>
+    <button className='bg-black text-white dark:bg-white dark:text-black rounded-md px-3 py-1' onClick={handleClick}>Okay</button>
     </div>
   )
 }
