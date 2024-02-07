7f051c0c7a75fdabb5a7236eb992c628958c6654
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2024-02-07 21:21:51 +0530

    Update Profile.jsx

--- frontend/src/components/profile/Profile.jsx
+++ frontend/src/components/profile/Profile.jsx
@@ -31,7 +31,7 @@ const Profile = ({showProfile , user}) => {
         return <Help showProfile={showProfile} setSelected={setSelected} />
     }
   return (
-    <div className={` ${showProfile? 'h-1/2 w-1/2 md:w-1/4  py-10 px-4 ':'w-0 h-0 p-0'} transition-all duration-500 ease-in-out absolute top-24 right-0 mx-2 rounded-lg  text-white bg-blue-900/80 dark:bg-emerald-800/80
+    <div className={` ${showProfile? 'h-1/2 w-2/3 md:w-1/4  py-10 px-4 ':'w-0 h-0 p-0'} transition-all duration-500 ease-in-out absolute top-24 right-0 mx-2 rounded-lg  text-white bg-blue-900/80 dark:bg-emerald-800/80
     flex justify-center items-start flex-col text-2xl z-10`}>
     {logoutModal ? 
     <LogoutModal  setLogoutModal={setLogoutModal} logout={logout} showProfile={showProfile} />
