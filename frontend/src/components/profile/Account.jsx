6d482ac2ba37c5d56e34c9a8652a4e94dfc73648
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2024-02-07 21:28:08 +0530

    Update Account.jsx

--- frontend/src/components/profile/Account.jsx
+++ frontend/src/components/profile/Account.jsx
@@ -9,7 +9,7 @@ const Account = ({showProfile , user ,setSelected}) => {
     const [save , setSave] = useState(false)
     const [isDisabled , setIsDisabled] = useState(true)
   return (
-    <div className={` ${showProfile? 'h-1/2 w-1/2 md:w-1/4 p-4 ':'w-0 h-0 p-0'} transition-all duration-500 ease-in-out  absolute top-24 right-0 mx-2 rounded-lg  text-white bg-blue-900/80 dark:bg-emerald-800/80
+    <div className={` ${showProfile? 'h-1/2 w-2/3 md:w-1/4 p-4 ':'w-0 h-0 p-0'} transition-all duration-500 ease-in-out  absolute top-24 right-0 mx-2 rounded-lg  text-white bg-blue-900/80 dark:bg-emerald-800/80
         flex justify-center items-start flex-col text-2xl z-10`}>
             <div className={`${showProfile ? 'block' : 'hidden'}  w-full`}>
 
